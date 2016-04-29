122cb58b54e4b5653935a6f6e644d52c3eee558b
Andrew Cattau <andrew.cattau@period7.com>
2016-04-29 11:04:48 -0600

    Add output options to command line

--- punq.c
+++ punq.c
@@ -3,6 +3,21 @@
 #include <string.h>
 #include <glib.h>
 
+static struct {
+	gboolean count;
+	gboolean comma;
+	gboolean tab;
+	gboolean after;
+} Punq_options;
+
+static GOptionEntry goptions[] = {
+	{ "count", 'c', 0, G_OPTION_ARG_NONE, &Punq_options.count, "Count occurrences of each line", NULL },
+	{ "comma", ',', 0, G_OPTION_ARG_NONE, &Punq_options.comma, "Use comma to separate count from text", NULL },
+	{ "tab",   't', 0, G_OPTION_ARG_NONE, &Punq_options.tab,   "Use tab to separate count from text", NULL },
+
+	{ NULL }
+};
+
 void count_lines(char *file, GHashTable *count)
 {
 	gchar *line;
@@ -22,15 +37,39 @@ void count_lines(char *file, GHashTable *count)
 	}
 }
 
-void print_fn(gpointer key, gpointer value, gpointer ignored) {
-	printf("%7d %s", *(int *)value, key);
+void print_fn(gpointer key, gpointer value, gpointer format) {
+	printf(format, *(int *)value, key);
 }
 
 void print_counts(GHashTable *count) {
-	g_hash_table_foreach(count, print_fn, NULL);
+	char *format;
+	if (Punq_options.count) {
+		if (Punq_options.comma) { 
+			format = "%d,%s";
+		} else if (Punq_options.tab) {
+			format = "%d\t%s";
+		} else {
+			format = "%7d %s";
+		}
+	} else {
+		format = "%2$s";
+	}
+	g_hash_table_foreach(count, print_fn, format);
 }
 
 int main (int argc, char *argv[]) {
+	GError *error = NULL;
+	GOptionContext *context;
+
+	context = g_option_context_new("- list unique lines in input files");
+	g_option_context_add_main_entries(context, goptions, NULL);
+	if (g_option_context_parse(context, &argc, &argv, &error)) {
+		g_option_context_free(context);
+	} else {
+		g_print("%s\n", g_option_context_get_help(context, TRUE, NULL));
+		return 1;
+	}
+
 	GHashTable *counts = g_hash_table_new(g_str_hash, (GEqualFunc)g_str_equal);
 	for (int i = 1; i < argc; i++) {
 		count_lines(argv[i], counts);
